07e79c8e358f7dcaf18b6c89a659cb5086e6b540
adeleon <adeleon@linode.com>
2019-01-27 13:13:46 -0500

    set up test and kiosk routes

--- api/index.js
+++ api/index.js
@@ -1,12 +1,71 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+require('./models/Test')
+require('./models/Kiosk')
 
 const app = new express()
+app.use(bodyParser.json())
 
-mongoose.connect('mongodb://mongodb:27017')
+const Test = mongoose.model('test')
+const Kiosk = mongoose.model('kiosk')
+
+mongoose.connect(
+  'mongodb://localhost:27017/indego',
+  () => {
+    console.log('Connected to mongo')
+  }
+)
 
 app.get('/', (req, res) => {
-  res.send('Hello there')
+  res.send('Indego')
+})
+
+app.post('/test', async (req, res) => {
+  const { first_name, last_name } = req.body
+
+  const test = new Test({
+    first_name,
+    last_name,
+  })
+
+  try {
+    const result = await test.save()
+    res.send(result)
+  } catch (err) {
+    res.status(422).send(err)
+  }
+})
+
+app.post('/testMany', async (req, res) => {
+  try {
+    const result = await Test.insertMany(req.body, { ordered: false })
+    res.send(result)
+  } catch (err) {
+    res.status(422).send(err)
+  }
+})
+
+app.post('/kiosk', async (req, res) => {
+  const kiosk = new Kiosk({
+    ...req.body,
+  })
+
+  try {
+    const result = await kiosk.save()
+    res.send(result)
+  } catch (err) {
+    res.status(422).send(err)
+  }
+})
+
+app.post('/kiosks', async (req, res) => {
+  try {
+    const result = await Kiosk.insertMany(req.body, { ordered: false })
+    res.send(result)
+  } catch (err) {
+    res.status(422).send(err)
+  }
 })
 
 const port = 4000
