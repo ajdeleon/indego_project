a2047af5f50fccb656deb10be0816f02cb96cf95
adeleon <adeleon@linode.com>
2019-02-08 15:54:14 -0500

    add timestamps

--- api/models/Kiosk.js
+++ api/models/Kiosk.js
@@ -1,16 +1,20 @@
 const mongoose = require('mongoose')
 const { Schema } = mongoose
 
-const kiosk = new Schema({
-  addressStreet: String,
-  addressZipCode: String,
-  bikesAvailable: Number,
-  docksAvailable: Number,
-  name: String,
-  latitude: Number,
-  longitude: Number,
-  kioskId: Number,
-  weather: { type: Schema.Types.ObjectId, ref: 'weather' },
-})
+const kiosk = new Schema(
+  {
+    addressStreet: String,
+    addressZipCode: String,
+    bikesAvailable: Number,
+    docksAvailable: Number,
+    name: String,
+    latitude: Number,
+    longitude: Number,
+    kioskId: Number,
+    weather: { type: Schema.Types.ObjectId, ref: 'weather' },
+  },
+
+  { timestamps: { createdAt: 'at' } }
+)
 
 mongoose.model('kiosk', kiosk)
