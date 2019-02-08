25641ea37d5e619696661a9c3a46250fde33277a
adeleon <adeleon@linode.com>
2019-02-08 15:57:26 -0500

    change response based on query params

--- api/routes/kioskRoutes.js
+++ api/routes/kioskRoutes.js
@@ -1,57 +1,60 @@
 const mongoose = require('mongoose')
 const Kiosk = mongoose.model('kiosk')
 const Weather = mongoose.model('weather')
+const parseParams = require('../middlewares/parseParams')
+const requireAt = require('../middlewares/requireAt')
 
 module.exports = app => {
-  app.get('/kiosk/:id', async (req, res) => {
-    const id = req.params.id
+  app.get('/stations', parseParams, requireAt, async (req, res) => {
     try {
-      const kiosk = await Kiosk.find({ kioskId: id })
-        .populate('weather')
-        .exec((err, results) => {
-          if (err) return console.log(err)
-          res.send(results)
-        })
-    } catch (err) {
-      res.status(422).send(err)
-    }
-  })
+      const kiosks = await Kiosk.find(res.locals.filter)
+      // const weather = await Weather.findById(kiosks[0].weather)
+      const weather = await Weather.find(res.locals.filter)
 
-  app.get('/station/:id', async (req, res) => {
-    const kioskId = req.params.id
-    try {
-      const kiosk = await Kiosk.findOne({ kioskId })
-      const weather = await Weather.findOne({ _id: kiosk.weather })
-
+      if (kiosks.length === 0)
+        return res.status(404).send('No data found for this time period')
       res.send({
         at: weather.at,
-        kiosk,
+        kiosks,
         weather,
       })
     } catch (err) {
-      res.status(422).send(err)
+      res.status(422).send(err.message)
     }
   })
 
-  app.post('/kiosk', async (req, res) => {
-    const kiosk = new Kiosk({
-      ...req.body,
-    })
-
+  app.get('/stations/:id', parseParams, async (req, res) => {
+    let response
+    const kioskId = req.params.id
     try {
-      const result = await kiosk.save()
-      res.send(result)
-    } catch (err) {
-      res.status(422).send(err)
-    }
-  })
+      const kiosks = await Kiosk.find({ kioskId, ...res.locals.filter })
+      const weather = await Weather.findById(kiosks[0].weather)
 
-  app.post('/kiosks', async (req, res) => {
-    try {
-      const result = await Kiosk.insertMany(req.body, { ordered: false })
-      res.send(result)
+      if (kiosks.length === 0)
+        return res.status(404).send('No data found for this time period')
+      //There is a better way to do this.
+      if (req.query.frequency === 'daily') {
+        const filteredKiosks = kiosks.filter(kiosk => {
+          const stationDate = new Date(kiosk.at)
+          return stationDate.getHours() === 12
+        })
+        return res.send(filteredKiosks)
+      }
+
+      if (req.query.to && req.query.from) {
+        response = kiosks.map((kiosk, index) => {
+          return { at: kiosk.at, station: kiosk, weather }
+        })
+      } else {
+        response = {
+          at: weather.at,
+          kiosk: [...kiosks],
+          weather,
+        }
+      }
+      res.send(response)
     } catch (err) {
-      res.status(422).send(err)
+      res.status(422).send(err.message)
     }
   })
 }
